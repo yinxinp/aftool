072580cbf9475c48f4afa2b3a73d8b47a47d0ba7
panafeng <yinxinpz@163.com>
2020-02-24 22:47:47 +0800

    aftool init

--- src/treetool.js
+++ src/treetool.js
@@ -1,19 +1,3 @@
-export function treeFilter(array) {
-  console.log("array==========>", array);
-  return array;
-}
-
-export function waitTime(func, delay = 0) {
-  return new Promise(
-    r => {
-      setTimeout(() => {
-        const result = func && func();
-        r(result);
-      }, delay);
-    },
-    rj => {
-      rj();
-      throw new Error("执行错误");
-    }
-  );
-}
+/**
+ * 树的基本操作
+ */
