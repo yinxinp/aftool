95fc8f477b5c5a3dca38bea8e4147af571413e25
panafeng <yinxinpz@163.com>
2020-02-24 22:37:00 +0800

    解决生产库打包时的小bug暂时注释es6-->es5的配置需要时在展开

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,12 +4,12 @@ const prodConfig = {
   entry: "./src/index.js",
   module: {
     rules: [
-      {
-        // 这边用babel将es6转换成es5   ===>如果没必要就不需要转换
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
+      // {
+      //   // 这边用babel将es6转换成es5   ===>如果没必要就不需要转换
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: "babel-loader"
+      // }
     ]
   },
   output: {
