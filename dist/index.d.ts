410f7f052df6b96a4877fa5bf1f49a9cdbf3fc66
dashabei <yinxinpz@163.com>
2020-04-07 09:24:47 +0800

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,5 @@ import treetool from "./treetool";
 import { StreamControl, Debounce } from "./streamControl";
 import { WaitAction } from "./waitAction";
 export { treetool, StreamControl, WaitAction, Debounce };
+export * as utils from "./utils";
+export * as domTool from "./domTool";
--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -3,3 +3,8 @@
  * @param param 参数对象
  */
 export declare function isAllNullorUndefined(param: any): boolean;
+/**
+ * 唯一标识生成器
+ * @param {String} seed 种子
+ */
+export declare function guid(seed: string): string;
