4c1547c964b84aa4aaba15c021f073a4c7cf41dd
dashabei <yinxinpz@163.com>
2020-04-07 09:23:51 +0800

    domTool的添加

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import treetool from "./treetool";
 import { StreamControl, Debounce } from "./streamControl";
 import { WaitAction } from "./waitAction";
 export { treetool, StreamControl, WaitAction, Debounce };
+
+export * as utils from "./utils";
+export * as domTool from "./domTool";
--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,10 @@
  * 判断参数对象的各个值是否为空或者undfined或者为null
  * @param param 参数对象
  */
-export function isAllNullorUndefined(param: any) :boolean{
+export function isAllNullorUndefined(param: any): boolean {
   let result = true;
   if (param) {
-    result = !Object.keys(param).some(x => {
+    result = !Object.keys(param).some((x) => {
       const property = param[x];
       return (
         (typeof property === "string" && property.trim() !== "") ||
@@ -16,4 +16,18 @@ export function isAllNullorUndefined(param: any) :boolean{
     });
   }
   return result;
+}
+
+/**
+ * 唯一标识生成器
+ * @param {String} seed 种子
+ */
+export function guid(seed: string) {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  seed = seed || "";
+  return `${s4()}${seed}${s4()}${seed}${s4()}${seed}${s4()}${seed}${s4()}${seed}${s4()}${seed}${s4()}${seed}${s4()}`;
 }
